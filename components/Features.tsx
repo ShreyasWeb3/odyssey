5d32016c5fbb39ee864590da8c523283553da5de
Shreyas M B <shreyasweb3@gmail.com>
2023-10-18 14:09:18 +0530

    Guide and Features section implemented

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,8 +1,66 @@
+import { FEATURES } from "@/constants";
+import Image from "next/image";
+
+interface FeatureProp {
+  title: string;
+  key: string;
+  icon: string;
+  description: string;
+}
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <section className=" flexCenter flex-col overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container flex padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone "
+          />
+        </div>
 
-export default Features
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="bold-40 lg:bold-64">Our features</h2>
+          </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                title={feature.title}
+                key={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const FeatureItem = ({ title , icon,description}: FeatureProp) => {
+  return <li className="flex flex-col w-full items-start">
+    <div className="rounded-full p-4 lg:p-7 bg-green-50">
+      <Image src={icon} alt={title} width={28} height={28} />
+    </div>
+    <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+      {title}
+    </h2>
+    <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      {description}
+    </p>
+  </li>;
+};
+
+export default Features;
